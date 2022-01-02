f6ab51aedaf778f6b312c398eba60f2f3fb91000
SiongSng <rrt467778@gmail.com>
2022-01-02 18:40:14 +0800

    Update main.dart

--- example/main.dart
+++ example/main.dart
@@ -6,7 +6,4 @@ import 'package:rpmtw_api_client/rpmtw_api_client.dart';
 
 void main() async {
   RPMTWApiClient apiClient = RPMTWApiClient(development: true);
-
-  await apiClient.storageResource.downloadStorage(
-      uuid: "12c7a662-a635-4b49-9578-96f831a8be0a", file: File("./test.txt"));
 }
