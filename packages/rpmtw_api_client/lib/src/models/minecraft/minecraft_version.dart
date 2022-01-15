09690b6e324040de90ccf2bb5c762ae575199dcd
SiongSng <rrt467778@gmail.com>
2022-01-15 10:07:59 +0800

    MinecraftVersion tostring

--- packages/rpmtw_api_client/lib/src/models/minecraft/minecraft_version.dart
+++ packages/rpmtw_api_client/lib/src/models/minecraft/minecraft_version.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:pub_semver/pub_semver.dart';
 import 'package:rpmtw_api_client/src/utilities/utility.dart';
 
@@ -22,18 +24,19 @@ class MinecraftVersion {
 
   Version get comparableVersion => Utility.parseMCComparableVersion(id);
 
-  MinecraftVersion(this.id, this.type, this.url, this.time, this.releaseTime,
-      this.sha1, this.complianceLevel);
+  const MinecraftVersion(this.id, this.type, this.url, this.time,
+      this.releaseTime, this.sha1, this.complianceLevel);
 
-  factory MinecraftVersion.fromMap(Map map) {
+  factory MinecraftVersion.fromMap(Map<String, dynamic> map) {
     return MinecraftVersion(
-        map['id'],
-        MinecraftVersionType.values.firstWhere((_) => _.name == map['type']),
-        map['url'],
-        map['time'],
-        map['releaseTime'],
-        map['sha1'],
-        map['complianceLevel']);
+      map['id'] ?? '',
+      MinecraftVersionType.values.firstWhere((_) => _.name == map['type']),
+      map['url'] ?? '',
+      map['time'] ?? '',
+      map['releaseTime'] ?? '',
+      map['sha1'] ?? '',
+      map['complianceLevel']?.toInt() ?? 0,
+    );
   }
 
   Map<String, dynamic> toMap() {
@@ -44,9 +47,64 @@ class MinecraftVersion {
       'time': time,
       'releaseTime': releaseTime,
       'sha1': sha1,
-      'complianceLevel': complianceLevel
+      'complianceLevel': complianceLevel,
     };
   }
+
+  @override
+  String toString() {
+    return 'MinecraftVersion(id: $id, type: $type, url: $url, time: $time, releaseTime: $releaseTime, sha1: $sha1, complianceLevel: $complianceLevel)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is MinecraftVersion &&
+        other.id == id &&
+        other.type == type &&
+        other.url == url &&
+        other.time == time &&
+        other.releaseTime == releaseTime &&
+        other.sha1 == sha1 &&
+        other.complianceLevel == complianceLevel;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        type.hashCode ^
+        url.hashCode ^
+        time.hashCode ^
+        releaseTime.hashCode ^
+        sha1.hashCode ^
+        complianceLevel.hashCode;
+  }
+
+  MinecraftVersion copyWith({
+    String? id,
+    MinecraftVersionType? type,
+    String? url,
+    String? time,
+    String? releaseTime,
+    String? sha1,
+    int? complianceLevel,
+  }) {
+    return MinecraftVersion(
+      id ?? this.id,
+      type ?? this.type,
+      url ?? this.url,
+      time ?? this.time,
+      releaseTime ?? this.releaseTime,
+      sha1 ?? this.sha1,
+      complianceLevel ?? this.complianceLevel,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MinecraftVersion.fromJson(String source) =>
+      MinecraftVersion.fromMap(json.decode(source));
 }
 
 enum MinecraftVersionType {
